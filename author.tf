40919d223b00ce99ce43a2f32a98e0ce0c72672d
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-09-08 14:53:40 +0100

    Add api and publisher

--- author.tf
+++ author.tf
@@ -6,7 +6,7 @@ resource "aws_alb_target_group" "author" {
 
   health_check = {
     healthy_threshold   = 2
-    unhealthy_threshold = 30
+    unhealthy_threshold = 10
     interval            = 5
     timeout             = 2
     path                = "/"
